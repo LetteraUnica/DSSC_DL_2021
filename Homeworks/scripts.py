e6844ec22e141a4352e845fb715425e231835548
letteraunica <lorenzocav97@gmail.com>
2021-04-07 21:58:04 +0200

    asd

--- Homeworks/scripts.py
+++ Homeworks/scripts.py
@@ -192,9 +192,10 @@ def plot_train_test(train_losses: list, test_losses: list):
     """
     import seaborn as sns
     sns.set_theme()
+    epochs = list(range(1, len(train_losses)+1))
     fig, axes = pl.subplots(1, 2, figsize=(12,4))
-    axes[0].plot(train_losses, label="train loss")
-    axes[1].plot(test_losses, label="test misclassification rate")
+    axes[0].plot(epochs, train_losses, label="train loss")
+    axes[1].plot(epochs, test_losses, label="test misclassification rate")
     axes[0].title.set_text("Training loss")
     axes[1].title.set_text("Test misclassification rate")
     axes[0].set_xlabel("Number of epochs")
